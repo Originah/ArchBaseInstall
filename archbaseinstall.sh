6f8896ebe9952caeb1b15a5f6e29af742f9f99d2
originah <jake@thebeehive.xyz>
2024-06-24 22:53:36 +0100

    Preparing main repo files

---
+++ archbaseinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ----------------------------------------------------- 
+# Arch Base Install Script, This is a control script.
+# This script will call other sub scripts located in
+# repo\scripts.
+# ----------------------------------------------------- 
